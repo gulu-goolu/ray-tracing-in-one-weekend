da6b0093a5856e939e2376eabc703703b4b6bb00
murmur-wheel <murmur.wheel@gmail.com>
2020-11-15 15:22:35 +0800

    chapter 5: adding a sphere

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
 #include "util.h"
 
-Vec3 color(const Ray& ray) {
+Vec3 background(const Ray& ray) {
   Vec3 n = normalize(ray.direction);
   Float t = (n.y + 1) * 0.5;
   return (1 - t) * Vec3(1) + t * Vec3(0.5, 0.7, 1.0);
+}
+
+class Sphere {
+ public:
+  Vec3 center;
+  Float radius;
+
+  bool hit(const Ray& ray) const {
+    Vec3 oc = ray.origin - center;
+    Float a = dot(ray.direction, ray.direction);
+    Float b = 2 * dot(oc, ray.direction);
+    Float c = dot(oc, oc) - radius * radius;
+    Float D = b * b - 4 * a * c;
+    return D > 0;
+  }
+};
+
+Vec3 color(const Ray& r) {
+  Sphere spehere{Vec3(0, 0, -1), 0.5};
+  if (spehere.hit(r)) {
+    return Vec3(1, 0, 0);
+  }
+
+  return background(r);
 }
 
 int main(int argc, char* argv[]) {
