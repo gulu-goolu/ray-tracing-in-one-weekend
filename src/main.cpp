e35935d5d4c81615bf65e89006b72f96488188a1
murmur-wheel <murmur.wheel@gmail.com>
2020-11-15 13:11:05 +0800

    Vec3, Bitmap

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
-int main(int argc, char* argv[]) {}
+#include "util.h"
+
+int main(int argc, char* argv[]) {
+  Bitmap bitmap(1024, 1024);
+  for (int i = 0; i < bitmap.rows; ++i) {
+    for (int j = 0; j < bitmap.cols; ++j) {
+      int x_grid = i / 32, y_grid = j / 32;
+      if ((x_grid + y_grid) % 2) {
+        bitmap[i][j] = Vec3(1);
+      } else {
+        bitmap[i][j] = Vec3(0);
+      }
+    }
+  }
+  save_as_png(bitmap, "board.png");
+}
---
+++ src/util.h
@@ -0,0 +1,101 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <cmath>
+#include <cstdint>
+#include <vector>
+
+using Float = float;
+
+struct Vec3 {
+  Float x, y, z;
+  Vec3() : x(0), y(0), z(0) {}
+  Vec3(Float a) : x(a), y(a), z(a) {}
+  Vec3(Float x_, Float y_, Float z_) : x(x_), y(y_), z(z_) {}
+
+  Vec3 operator-() const { return Vec3(-x, -y, -z); }
+  Float& operator[](int i) { return (&x)[i]; }
+  const Float& operator[](int i) const { return (&x)[i]; }
+
+  Vec3 operator+(const Vec3& rhs) const {
+    return Vec3(x + rhs.x, y + rhs.y, z + rhs.z);
+  }
+  Vec3 operator-(const Vec3& rhs) const {
+    return Vec3(x - rhs.x, y - rhs.y, z - rhs.z);
+  }
+  Vec3 operator*(const Vec3& rhs) const {
+    return Vec3(x * rhs.x, y * rhs.y, z * rhs.z);
+  }
+  Vec3 operator*(Float a) const { return Vec3(x * a, y * a, z * a); }
+  Vec3 operator/(const Vec3& rhs) const {
+    return Vec3(x / rhs.x, y / rhs.y, z / rhs.z);
+  }
+
+  Vec3& operator+=(const Vec3& rhs) {
+    x += rhs.x;
+    y += rhs.y;
+    z += rhs.z;
+    return *this;
+  }
+  Vec3& operator-=(const Vec3& rhs) {
+    x -= rhs.x;
+    y -= rhs.y;
+    z -= rhs.z;
+    return *this;
+  }
+  Vec3& operator*=(const Vec3& rhs) {
+    x *= rhs.x;
+    y *= rhs.y;
+    z *= rhs.z;
+    return *this;
+  }
+  Vec3& operator/=(const Vec3& rhs) {
+    x /= rhs.x;
+    y /= rhs.y;
+    z /= rhs.z;
+    return *this;
+  }
+};
+
+inline Float squared_length(const Vec3& v) {
+  return v.x * v.x + v.y * v.y + v.z * v.z;
+}
+
+inline Float length(const Vec3& v) { return std::sqrt(squared_length(v)); }
+
+inline Vec3 normalize(const Vec3& v) {
+  Float len = length(v);
+  return v * (1 / len);
+}
+
+inline Vec3 cross(const Vec3& v1, const Vec3& v2) {
+  Float x = v1.y * v2.z - v1.z * v2.y;
+  Float y = v1.z * v2.x - v1.x * v2.z;
+  Float z = v1.x * v2.y - v1.y * v2.x;
+  return Vec3(x, y, z);
+}
+
+struct Bitmap {
+  explicit Bitmap(int cols, int rows) : cols(cols), rows(rows) {
+    const int total = cols * rows;
+    data.resize(total);
+  }
+
+  Vec3* operator[](int i) {
+    const int pos = i * cols;
+    return &data[pos];
+  }
+
+  const Vec3* operator[](int i) const {
+    const int pos = i * cols;
+    return &data[pos];
+  }
+
+  const int cols, rows;
+  std::vector<Vec3> data;
+};
+
+// example: save_as_png(bitmap, "output.png");
+void save_as_png(const Bitmap& bitmap, const char* filename);
+
+#endif
